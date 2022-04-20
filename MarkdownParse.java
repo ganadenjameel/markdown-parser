57162530aa7cd52200b076da25021c376ac5e91d
ganadenjameel <103278077+ganadenjameel@users.noreply.github.com>
2022-04-19 19:15:57 -0700

    Image Error Fix

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -16,6 +16,11 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            // skip image read
+            if (markdown.charAt(openBracket - 1) == '!') {
+                currentIndex = closeParen + 1;
+                continue;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
