dba481d95d5f68088fb3aa202365d25700ab9858
ganadenjameel <103278077+ganadenjameel@users.noreply.github.com>
2022-04-19 19:20:34 -0700

    Missing Open Bracket Error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,7 +11,10 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while (currentIndex < markdown.length()) {
+        boolean check = true;
+
+        // if bracket does not exist, code will skip
+        while (currentIndex < markdown.length() && check) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
@@ -23,6 +26,9 @@ public class MarkdownParse {
             }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
+            if (markdown.indexOf("[", closeParen) == -1) {
+                check = false;
+            }
         }
 
         // throw an error if the file is empty
