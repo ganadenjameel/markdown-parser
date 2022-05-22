64bada970355fdb35c6432e9ff20644fc15f0ede
ganadenjameel <103278077+ganadenjameel@users.noreply.github.com>
2022-05-22 16:15:26 -0700

    update

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -26,7 +26,7 @@ public class MarkdownParseTest {
         String content = Files.readString(fileName);
         ArrayList<String> expected = new ArrayList<>();
         expected.add("a.com");
-        expected.add("a.com((");
+        expected.add("a.com(())");
         expected.add("example.com");
         assertEquals(expected, MarkdownParse.getLinks(content));
     }
@@ -36,9 +36,10 @@ public class MarkdownParseTest {
         Path fileName = Path.of("snippet3.md");
         String content = Files.readString(fileName);
         ArrayList<String> expected = new ArrayList<>();
-        expected.add("    https://www.twitter.com");
         expected.add("https://sites.google.com/eng.ucsd.edu/cse-15l-spring-2022/schedule");
-        expected.add("github.com And there's still some more text after that. [this link doesn't have a closing parenthesis for a while](https://cse.ucsd.edu/");
         assertEquals(expected, MarkdownParse.getLinks(content));
     }
+
+
+    
 }
