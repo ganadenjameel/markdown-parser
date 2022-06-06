8e47ed84db6c85ba2269a3718653ab0e1c701d7b
ganadenjameel <103278077+ganadenjameel@users.noreply.github.com>
2022-06-06 02:01:39 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,92 +1,50 @@
-// File reading code from https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class MarkdownParse {
 
-    static int findCloseParen(String markdown, int openParen) {
-        int closeParen = openParen + 1;
-        int openParenCount = 1;
-        while (openParenCount > 0 && closeParen < markdown.length()) {
-            if (markdown.charAt(closeParen) == '(') {
-                openParenCount++;
-            } else if (markdown.charAt(closeParen) == ')') {
-                openParenCount--;
-            }
-            closeParen++;
-        }
-        if(openParenCount == 0) {
-          return closeParen - 1;
-        }
-        else {
-          return -1;
-        }
-
-    }
-    public static Map<String, List<String>> getLinks(File dirOrFile) throws IOException {
-        Map<String, List<String>> result = new HashMap<>();
-        if(dirOrFile.isDirectory()) {
-            for(File f: dirOrFile.listFiles()) {
-                result.putAll(getLinks(f));
-                System.out.println("hi");
-            }
-            return result;
-        }
-        else {
-            Path p = dirOrFile.toPath();
-            int lastDot = p.toString().lastIndexOf(".");
-            if(lastDot == -1 || !p.toString().substring(lastDot).equals(".md")) {
-                return result;
-            }
-            ArrayList<String> links = getLinks(Files.readString(p));
-            result.put(dirOrFile.getPath(), links);
-            return result;
-        }
-    }
-    public static ArrayList<String> getLinks(String markdown) {
+    public static ArrayList<String> getLinks(String markdown) throws IOException {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
+        // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
-            int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            int nextCodeBlock = markdown.indexOf("\n```");
-            if(nextCodeBlock < nextOpenBracket && nextCodeBlock != -1) {
-                int endOfCodeBlock = markdown.indexOf("\n```");
-                currentIndex = endOfCodeBlock + 1;
-                continue;
+            int openBracket = markdown.indexOf("[", currentIndex);
+            int closeBracket = markdown.indexOf("]", openBracket);
+            int openParen = markdown.indexOf("(", closeBracket);
+            int closeParen = markdown.indexOf(")", openParen);
+            if (openBracket > 0) {
+                if (markdown.charAt(openBracket-1) == '!')
+                {
+                    currentIndex = closeParen + 1;
+                    continue;
+                }
             }
-            int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-            int openParen = markdown.indexOf("(", nextCloseBracket);
-
-            // The close paren we need may not be the next one in the file
-            int closeParen = findCloseParen(markdown, openParen);
-            
-            if(nextOpenBracket == -1 || nextCloseBracket == -1
-                  || closeParen == -1 || openParen == -1) {
-                return toReturn;
+            if (openBracket == -1) {
+                throw new IllegalArgumentException("Invalid Input!");
             }
-            String potentialLink = markdown.substring(openParen + 1, closeParen).trim();
-            if(potentialLink.indexOf(" ") == -1 && potentialLink.indexOf("\n") == -1) {
-                toReturn.add(potentialLink);
-                currentIndex = closeParen + 1;
+            if (openParen == -1) {
+                throw new IllegalArgumentException("Invalid Input!");
             }
-            else {
-                currentIndex = currentIndex + 1;
+            if (closeBracket < openBracket) {
+                throw new IllegalArgumentException("Invalid Input!");
             }
+            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            currentIndex = closeParen + 1;  
         }
+        if (markdown.length() == 0) {
+            throw new IOException("Empty File!");
+        }
+        //System.out.println("THIS IS CURRENT INDEX: " + currentIndex);
         return toReturn;
     }
+
+
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
-        String contents = Files.readString(fileName);
-        ArrayList<String> links = getLinks(contents);
-        System.out.println(links);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+	    System.out.println(links);
     }
 }
